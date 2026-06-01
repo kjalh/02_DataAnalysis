05539c57fe2ef4bf852b82d3e9fee471901d2488
희승ACMY <heseung0895@naver.com>
2026-06-01 17:07:25 +0900

    기본 구조

---
+++ 링크드_리스트(Linked_List).py
@@ -0,0 +1,26 @@
+class Node:
+    def __init__(self):
+        self.data 
+        self.next
+    
+
+
+class LinkedList:
+    def __init__(self):
+        pass
+
+    def in_data():
+        pass
+
+    def out_data():
+        pass
+
+    def find():
+        pass
+
+
+lin = Node
+
+
+
+
