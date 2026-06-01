7ed0be8bf20c2cdaaf4cd584894b8749656a221f
희승ACMY <heseung0895@naver.com>
2026-06-01 17:46:56 +0900

    링크드 리스트 중간

--- 링크드_리스트(Linked_List).py
+++ 링크드_리스트(Linked_List).py
@@ -1,30 +1,57 @@
 class Node:
     def __init__(self):
-        self.data 
-        self.next
+        self.node = dict(self.n_data, self.next)
+        self.head
+        self.tail = None
+        
 
-    def node():
-        pass
     
 
 
-class LinkedList:
+class LinkedList(Node):
     def __init__(self):
+        self.data
+            
         pass
 
-    def in_data():
+    def in_data(self):
+        self.data = input("추가 값 입력:")
+        
+        
         pass
 
     def out_data():
         pass
 
-    def find():
+    def find_data():
         
+        m = int(input("전체조회(1)/원하는 값 조회(2): "))
+        
+        if m == 1:
+            pass
+
+
         while True if next != None else False:
             pass
         
         pass
 
 
-lin = Node
+
+node = Node
+lin = LinkedList
+
+menu = input("apnd/del/find : ")
+
+while True:
+    if menu == "apnd":
+        lin.in_data()
+    elif menu == "del":
+        lin.out_data()
+    elif menu == "find":
+        lin.find_data()
+    elif menu == "0":
+        exit()
+    else:
+        print("다시 입력")
 
